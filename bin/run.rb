475c313cd1fd588ecdca4527f1bf8e682a98bd9d
Alexander Schelchere <ascellie@gmail.com>
2019-06-20 10:07:32 -0400

    Music, throu system

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,14 @@
 require_relative '../config/environment'
 require_relative '../api/alcoholic_cocktail_api'
 
+def sound(file_path)
+    system "mpg123 #{file_path}"
+end
 
+sound("audio/T-Pain-buy_u_a_drank.mp3")
 tty_home
 
 
 binding.pry
+
 0
