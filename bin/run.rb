7113f56f24b9adc85588b1294f155572347bd2a6
Alexander Schelchere <ascellie@gmail.com>
2019-06-20 10:48:44 -0400

    We got our music working!

--- bin/run.rb
+++ bin/run.rb
@@ -2,12 +2,12 @@ require_relative '../config/environment'
 require_relative '../api/alcoholic_cocktail_api'
 
 def sound(file_path)
-    system "mpg123 #{file_path}"
+    pid = fork{ exec 'mpg123','-q', file_path }
 end
 
 sound("audio/T-Pain-buy_u_a_drank.mp3")
 tty_home
-
+sound("audio/semisonic-closing_time.mp3")
 
 binding.pry
 
